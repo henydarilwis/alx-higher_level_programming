e52c078c850b62630853551299eeff757989bca8
Henok &  Yewelsew <Henok>
2023-06-11 06:16:03 -0700

    hot fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return (None)
-    else:
-        max_int = my_list[0]
-        for i in range(len(my_list)):
-            if my_list[i] > max_int:
-                max_int = my_list[i]
-        return (max_int)
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print('{:d}'.format(column), end='')
+            else:
+                print('{:d}'.format(column), end=' ')
+        print()

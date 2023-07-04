a88487cace1e59dbf9aaf20e0d66fa75636d9a5b
Henok &  Yewelsew <Henok>
2023-07-04 09:35:42 -0700

    first

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+"""Rectangle Class by Henok.
+
+This module contains an empty class that defines a rectangle.
+
+Usage Example:
+
+    Rectangle = __import__('0-rectangle').Rectangle
+
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
+"""
+
+
+class Rectangle:
+    """Defines the blueprint of a rectangle"""
+
+    pass

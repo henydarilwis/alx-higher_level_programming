a0a5ad55c57b7cdf971506273373bdef30f991cd
Henok &  Yewelsew <Henok>
2023-07-04 10:20:49 -0700

    hot fixss

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle...
+"""Solves the N-queens puzzle.
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
 Example:
@@ -10,7 +10,7 @@ Attributes:
     solutions (list): A list of lists containing solutions.
 Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
 where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard....
+queen must be placed on the chessboard.
 """
 import sys
 
